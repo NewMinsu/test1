7763f1be2eb5322ef22701a95cbd91b7a6cb2731
juwon <80947350+1229juwon@users.noreply.github.com>
2022-12-05 17:55:01 +0900

    test
    test

---
+++ src/test/java/test/Test.java
@@ -0,0 +1,18 @@
+package test;
+
+import dev.mvc.contents.ContentsVO;
+
+public class Test {
+
+  public static void main(String[] args) {
+    String str = "";
+    String str2 = null;
+    
+    str.toUpperCase();
+    str2.toUpperCase();
+    
+    ContentsVO contentsVO = new ContentsVO();
+    
+  }
+
+}
